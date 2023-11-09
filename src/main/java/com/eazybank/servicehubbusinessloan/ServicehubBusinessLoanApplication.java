0d8b3bd355351777e1ea732793f51e60ac430082
nahakritesh06 <nahakritesh06@gmail.com>
2023-11-09 23:45:45 +0530

    initial commit

---
+++ src/main/java/com/eazybank/servicehubbusinessloan/ServicehubBusinessLoanApplication.java
@@ -0,0 +1,13 @@
+package com.eazybank.servicehubbusinessloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicehubBusinessLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicehubBusinessLoanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eazybank/servicehubbusinessloan/ServicehubBusinessLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybank.servicehubbusinessloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicehubBusinessLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
